5ae58c6c1facaabdcf5f32817d7b69b05a100ce8
webdev522 <webdev522@gmail.com>
2019-04-20 01:42:29 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import SendToDropbox from './components/SendToDropbox';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        {/* <header className="App-header">
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
@@ -19,7 +18,8 @@ class App extends Component {
           >
             Learn React
           </a>
-        </header>
+        </header> */}
+        <SendToDropbox />
       </div>
     );
   }
